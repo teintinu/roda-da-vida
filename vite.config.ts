9e373e0622b2bfc5b61e3c570015c6960d995c3b
José Fernando Tolentino <josefernandotolentino@gmail.com>
2024-01-24 18:58:52 +0000

    fix base on vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/roda-da-vida/',
   plugins: [react()],
   build: {
     outDir: 'docs',
     emptyOutDir: true,
-    assetsDir: '/roda-da-vida/assets',
   },
 })
